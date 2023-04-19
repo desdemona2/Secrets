277252b0ec71956f5a0dce9360ab7ffa34a947e4
Bhavuk <bhavuksharma2202@gmail.com>
2023-04-19 15:44:37 +0530

    Level 2 Security
    Passwords will be encrypted in database

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const parser = require('body-parser');
 const ejs = require('ejs');
 const path = require('path');
 
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
 const app = express();
 
 // Path to static files (javascript or css)
@@ -15,6 +19,15 @@ app.set('view engine', 'ejs');
 // To parse url encoded response from post request
 app.use(parser.urlencoded({extended: true}));
 
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "SuperSecretString";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+const User = mongoose.model("User", userSchema);
 
 app.route('/')
     .get(async function(req, res) {
@@ -26,10 +39,39 @@ app.route('/login')
     .get(async function(req, res) {
         res.render('login');
     })
+    .post(async function(req, res) {
+        User.findOne({
+            email: req.body.username,
+        }).then(doc => {
+            console.log(doc)
+            if (doc != null && doc.password === req.body.password) {
+                res.render('secrets');
+            } else {
+                res.send("Username/Password you entered is wrong!");
+            }
+        }).catch(err => {
+            console.log("Error: " +err);
+            res.send("Some error occured while finding your record!")
+        });
+    })
 
 app.route('/register')
     .get(async function(req, res) {
         res.render('register');
+    })
+    .post(async function(req, res) {
+        new User({
+            email: req.body.username,
+            password: req.body.password
+        }).save()
+            .then(doc => {
+                console.log(doc);
+                res.render('secrets');
+            })
+            .catch(err => {
+                console.log("Some error occured! Error: " + err);
+                res.send("Error occured while adding user: " + err);
+            });
     })
 
 
